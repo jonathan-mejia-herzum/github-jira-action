4a82dd55a0024e4bd538dc422b1760528dc0d44f
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-06-23 12:36:07 -0500

    fix test

--- index.js
+++ index.js
@@ -4,72 +4,69 @@ const fetch = require('node-fetch');
 const body = require('./body');
 
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  const jiraBaseUrl = core.getInput('JIRA_BASE_URL');
-  const jiraUserEmail = core.getInput('JIRA_USER_EMAIL');
-  const jiraApiToken = core.getInput('JIRA_API_TOKEN');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const commit = github.context.payload.commits[0];
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const message = commit.message;
 
-  const indexOfFirst = message.indexOf('\n');
+// `who-to-greet` input defined in action metadata file
+const nameToGreet = core.getInput('who-to-greet');
+const jiraBaseUrl = core.getInput('JIRA_BASE_URL');
+const jiraUserEmail = core.getInput('JIRA_USER_EMAIL');
+const jiraApiToken = core.getInput('JIRA_API_TOKEN');
+console.log(`Hello ${nameToGreet}!`);
+const time = (new Date()).toTimeString();
+core.setOutput("time", time);
+// Get the JSON webhook payload for the event that triggered the workflow
+const commit = github.context.payload.commits[0];
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+const message = commit.message;
 
-  let words;
-  if (indexOfFirst > 0) {
-    words = message.split('\n');
-  }
+const indexOfFirst = message.indexOf('\n');
 
-
-  words = words[0].split(' ');
-  const issue = words[0];
-
-
-  const bodyJson = body.getBody(commit.url, message);
-  const url = `${jiraBaseUrl}/rest/api/3/issue/${issue}/comment`;
-
-
-  console.log(`jira TOken ${jiraApiToken}`);
-  console.log(`jira jiraBaseUrl: ${jiraBaseUrl}`);
-  console.log(`jira jira email: ${jiraUserEmail}`);
-  console.log(`This is the commit ${issue}`);
-  console.log(`This is the url ${url}`);
-
-  console.log(JSON.stringify(bodyJson, undefined, 2))
-  console.log(`This is the payload ${payload}`);
-
-
-
-
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(
-        `${jiraUserEmail}:${jiraApiToken}`
-      ).toString('base64')}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(bodyJson, undefined, 2)
-  })
-    .then(response => {
-      console.log(
-        `Response: ${response.status} ${response.statusText}`
-      );
-      return response.text();
-    })
-    .then(text => console.log(text))
-    .catch(err => console.error(err));
-
-
-
-} catch (error) {
-  core.setFailed(error.message);
+let words;
+if (indexOfFirst > 0) {
+  words = message.split('\n');
 }
+
+
+words = words[0].split(' ');
+const issue = words[0];
+
+
+const bodyJson = body.getBody(commit.url, message);
+const url = `${jiraBaseUrl}/rest/api/3/issue/${issue}/comment`;
+
+
+console.log(`jira TOken ${jiraApiToken}`);
+console.log(`jira jiraBaseUrl: ${jiraBaseUrl}`);
+console.log(`jira jira email: ${jiraUserEmail}`);
+console.log(`This is the commit ${issue}`);
+console.log(`This is the url ${url}`);
+
+console.log(JSON.stringify(bodyJson, undefined, 2))
+console.log(`This is the payload ${payload}`);
+
+
+
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Authorization': `Basic ${Buffer.from(
+      `${jiraUserEmail}:${jiraApiToken}`
+    ).toString('base64')}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(bodyJson, undefined, 2)
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    return response.text();
+  })
+  .then(text => console.log(text));
+
+
+
+
 
 
