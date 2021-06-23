5296e7016da282c976708cbe0163206c31ef669f
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-06-23 17:06:00 -0500

    fix ne

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const body = require('./body');
 const jiraBaseUrl = core.getInput('JIRA_BASE_URL');
 const jiraUserEmail = core.getInput('JIRA_USER_EMAIL');
 const jiraApiToken = core.getInput('JIRA_API_TOKEN');
+const event = core.getInput('event');
 const time = (new Date()).toTimeString();
 core.setOutput("time", time);
 const payload = JSON.stringify(github.context.payload, undefined, 2);
-console.log(payload);
+console.log(`thi is the event ${event}`);
+console.log(`This is payload:  ${payload}`)
+
+
 
 
 
