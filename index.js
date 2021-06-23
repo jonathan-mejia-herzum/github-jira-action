40ed7e85715b0f2cfb01ec5aac3d47e231e0ddcd
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-06-23 09:09:36 -0500

    get issue from commit

--- index.js
+++ index.js
@@ -13,22 +13,61 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  const message = payload.commits[0].message;
+  const words = message.split(' ');
+  const issue = words[0];
+
   console.log(`jira TOken ${jiraApiToken}`);
   console.log(`jira jiraBaseUrl: ${jiraBaseUrl}`);
   console.log(`jira jira email: ${jiraUserEmail}`);
   console.log(`The event payload: ${payload}`);
+  console.log(`This is the commit ${issue}`);
 
 
 
 
-fetch(`${jiraBaseUrl}/rest/api/3/issue/SSD-9`, {
-  method: 'GET',
+
+
+
+
+/*
+// This code sample uses the 'node-fetch' library:
+// https://www.npmjs.com/package/node-fetch
+const fetch = require('node-fetch');
+
+const bodyData = `{
+  "visibility": {
+    "type": "role",
+    "value": "Administrators"
+  },
+  "body": {
+    "type": "doc",
+    "version": 1,
+    "content": [
+      {
+        "type": "paragraph",
+        "content": [
+          {
+            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper.",
+            "type": "text"
+          }
+        ]
+      }
+    ]
+  }
+}`;
+
+fetch(`${jiraBaseUrl}/rest/api/3/issue/{issueIdOrKey}/comment`, {
+  method: 'POST',
   headers: {
     'Authorization': `Basic ${Buffer.from(
-      `${jiraUserEmail}:${jiraApiToken}`
+      'email@example.com:<api_token>'
     ).toString('base64')}`,
-    'Accept': 'application/json'
-  }
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: bodyData
 })
   .then(response => {
     console.log(
@@ -38,12 +77,5 @@ fetch(`${jiraBaseUrl}/rest/api/3/issue/SSD-9`, {
   })
   .then(text => console.log(text))
   .catch(err => console.error(err));
-
-
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-
-
+*/
 
