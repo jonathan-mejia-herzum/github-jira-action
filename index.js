78df8f30cbe3748d73973bed78133806d89a35e7
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-06-23 17:37:41 -0500

    Test 123

--- index.js
+++ index.js
@@ -13,7 +13,53 @@ const payload = JSON.stringify(github.context.payload, undefined, 2);
 console.log(`thi is the event ${event}`);
 console.log(`This is payload:  ${payload}`)
 
+let commit, message;
 
+if (event == 'release') {
+  message = github.context.payload.release.name;
+}
+else {
+
+  commit = github.context.payload.head_commit;
+  message = commit.message;
+
+}
+
+
+const indexOfFirst = message.indexOf('\n');
+
+let words;
+if (indexOfFirst > 0) {
+  words = message.split('\n');
+}
+
+
+words = words[0].split(' ');
+const issue = words[0];
+
+
+const bodyJson = body.getBody(commit.url, message);
+const url = `https://${jiraBaseUrl}.atlassian.net/rest/api/3/issue/${issue}/comment`;
+
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Authorization': `Basic ${Buffer.from(
+      `${jiraUserEmail}:${jiraApiToken}`
+    ).toString('base64')}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(bodyJson, undefined, 2)
+})
+  .then(response => {
+    if (response.status != 201) {
+      core.setFailed(response.statusText);
+    }
+    console.log(`Response: ${response.status}`);
+    return 'OK';
+  });
 
 
 
