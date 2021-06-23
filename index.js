99e108468c4c4871915c2215bcac7994ff0a336c
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-06-23 16:46:51 -0500

    test

--- index.js
+++ index.js
@@ -8,43 +8,8 @@ const jiraUserEmail = core.getInput('JIRA_USER_EMAIL');
 const jiraApiToken = core.getInput('JIRA_API_TOKEN');
 const time = (new Date()).toTimeString();
 core.setOutput("time", time);
-const commit = github.context.payload.commits[0];
-const message = commit.message;
-
-const indexOfFirst = message.indexOf('\n');
-
-let words;
-if (indexOfFirst > 0) {
-  words = message.split('\n');
-}
-
-
-words = words[0].split(' ');
-const issue = words[0];
-
-
-const bodyJson = body.getBody(commit.url, message);
-const url = `https://${jiraBaseUrl}.atlassian.net/rest/api/3/issue/${issue}/comment`;
-
-
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Authorization': `Basic ${Buffer.from(
-      `${jiraUserEmail}:${jiraApiToken}`
-    ).toString('base64')}`,
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(bodyJson, undefined, 2)
-})
-  .then(response => {
-    if(response.status != 201){
-      core.setFailed(response.statusText);
-    }
-    console.log(`Response: ${response.status}`);
-    return 'OK';
-  });
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+console.log(payload);
 
 
 
