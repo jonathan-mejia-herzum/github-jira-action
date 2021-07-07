98a97558da9831874c8d1a63840729e7b27b443f
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-07-07 17:17:08 -0500

    add token

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const jiraBaseUrl = core.getInput('JIRA_BASE_URL');
 const jiraUserEmail = core.getInput('JIRA_USER_EMAIL');
 const jiraApiToken = core.getInput('JIRA_API_TOKEN');
 const idField = core.getInput('idField');
+const token = core.getInput('token');
+
 const event = core.getInput('event');
 const sha = core.getInput('sha');
 const time = (new Date()).toTimeString();
@@ -16,7 +18,7 @@ const payloadtoken = JSON.stringify(github.token, undefined, 2);
 
 console.log(payload);
 console.log('****token****')
-console.log(payloadtoken);
+console.log(token);
 console.log('********')
 
 let commit, url, message;
