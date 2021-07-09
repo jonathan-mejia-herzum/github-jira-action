4829d1e92db4bd22ef4e2d808967c5d94c10e116
Jonathan Mejia <jonathan.mejia@herzum.com>
2021-07-09 15:44:10 -0500

    get branch

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const event = core.getInput('event');
 const sha = core.getInput('sha');
 const time = (new Date()).toTimeString();
 core.setOutput("time", time);
-const payload = JSON.stringify(github, undefined, 2);
+const payload = JSON.stringify(github.payload, undefined, 2);
 const payloadtoken = JSON.stringify(github.token, undefined, 2);
 
+const ref = 'refs/heads/create-comment';
+const refSplit = ref.split('/');
+const branch = refSplit[refSplit.length-1];
+console.log('***********BRANCH  or TAG');
+console.log(branch)
 console.log(payload);
 console.log('****token****')
 console.log(token);
